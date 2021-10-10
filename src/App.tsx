c98ddeb98978b7d1c85db87321354b6e6b67721e
Vladimir Dabić <dabic.vladimir@gmail.com>
2021-10-10 13:36:32 +0200

    header component
    * lato font
    * remove cra boilerplate
    * cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
+import Header from './header/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div>
+    <Header title="Acme locations" subtitle="All locations" />
+  </div>
+);
 
 export default App;
